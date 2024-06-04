90d4ce99fed2f544a2c2bf689662226fb0ac42a0
Edna Sung <esung@lcs.on.ca>
2024-06-04 09:55:46 -0400

    Made a button for going back.

--- StaticInterface/Views/BackView.swift
+++ StaticInterface/Views/BackView.swift
@@ -9,7 +9,22 @@ import SwiftUI
 
 struct BackView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        HStack {
+            
+            Image(systemName: "arrow.left")
+                .foregroundColor(.white)
+                .font(Font.custom("Arial-BoldMT", size: 20))
+                .padding(.leading, 20)
+             
+            Text("Back")
+                .foregroundColor(.white)
+                .font(Font.custom("Arial-BoldMT", size: 20))
+            
+            Spacer()
+        }
+        
+        
     }
 }
 
