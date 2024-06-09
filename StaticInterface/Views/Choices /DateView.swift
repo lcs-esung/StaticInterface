5dddc625d4b19b5b94a6203d93220c8d7e05c416
Edna Sung <esung@lcs.on.ca>
2024-06-09 14:15:24 -0400

    Made questions on weather, days, and dates.

---
+++ StaticInterface/Views/Choices /DateView.swift
@@ -0,0 +1,54 @@
+//
+//  DateView.swift
+//  StaticInterface
+//
+//  Created by Edna Sung on 2024-06-09.
+//
+
+import SwiftUI
+
+struct DateView: View {
+    
+    let gradientColor = Gradient(colors: [.lightBlue, .lightGrey])
+
+    var body: some View {
+
+        let lightGrey = Color(
+                   hue: 0/360.0,
+                   saturation: 0.0,
+                   brightness: 0.93
+               )
+
+        let gradient2 = LinearGradient(gradient: gradientColor, startPoint: UnitPoint(x: 1, y: 0.5), endPoint: UnitPoint(x: 1, y: 1))
+
+        ZStack{
+            Rectangle()
+                .foregroundStyle(gradient2)
+                .ignoresSafeArea()
+            
+            VStack{
+                
+                BackView()
+                
+                Spacer()
+                
+                TitleView()
+                    .padding(.bottom, 80)
+                
+                QuestionView(question: "Which dates are you going to be travelling? (Start and end)")
+                
+                TextBoxView(textFieldAnswer: "Enter answer here")
+                  
+                Spacer()
+                  
+                
+             
+            }
+        }
+    }
+}
+
+
+#Preview {
+    DateView()
+}
