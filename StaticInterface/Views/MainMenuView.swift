3bc5912969807964f09197b3e230830fb0d25402
Edna Sung <esung@lcs.on.ca>
2024-06-13 12:19:36 -0400

    Made better spacing for main menu view.

--- StaticInterface/Views/MainMenuView.swift
+++ StaticInterface/Views/MainMenuView.swift
@@ -10,19 +10,19 @@ import SwiftUI
 struct MainMenuView: View {
     
     let gradientColor = Gradient(colors: [.lightBlue, .lightGrey])
-
+    
     var body: some View {
         
         let lightGrey = Color(
-                   hue: 0/360.0,
-                   saturation: 0.0,
-                   brightness: 0.93
-               )
+            hue: 0/360.0,
+            saturation: 0.0,
+            brightness: 0.93
+        )
         
         let gradient2 = LinearGradient(gradient: gradientColor, startPoint: UnitPoint(x: 1, y: 0.5), endPoint: UnitPoint(x: 1, y: 1))
         
         NavigationStack {
-
+            
             ZStack{
                 
                 Rectangle()
@@ -45,7 +45,7 @@ struct MainMenuView: View {
                             .padding(.leading, 30)
                         
                         Spacer()
-                    
+                        
                     }
                     
                     HStack {
@@ -64,51 +64,41 @@ struct MainMenuView: View {
                             .foregroundColor(.darkBlue)
                             .frame(height: 150)
                             .padding(.leading, 30)
-                
-                   
                         
                         Spacer()
                         
-                           
                     }
-                   
+                    
                     Spacer()
                         .padding()
-                  
                     
                     NavigationLink {
                         FirstQuestionView()
+                        
                     } label: {
+                        
                         ZStack {
+                            
                             Rectangle()
                                 .foregroundStyle(.darkBlue)
                                 .frame(width: 150)
                                 .cornerRadius(120)
                             
-                            
                             Text("Get Started")
                                 .foregroundColor(.white)
-                            
                         }
                         
                     }
-
-                    
-
                     Spacer()
                         .padding()
-                    
-                }
-                    
-                
-                
                 }
             }
-
         }
-       
+        
+    }
     
-  
+    
+    
     
 }
 
