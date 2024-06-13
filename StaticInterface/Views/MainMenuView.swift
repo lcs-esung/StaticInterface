73b97fc99850e19efcb1dac27345da709fb2450a
Edna Sung <esung@lcs.on.ca>
2024-06-13 12:09:34 -0400

    Made it so the main menu button is linked to the first question view.

--- StaticInterface/Views/MainMenuView.swift
+++ StaticInterface/Views/MainMenuView.swift
@@ -21,78 +21,90 @@ struct MainMenuView: View {
         
         let gradient2 = LinearGradient(gradient: gradientColor, startPoint: UnitPoint(x: 1, y: 0.5), endPoint: UnitPoint(x: 1, y: 1))
         
-        ZStack{
-            
-            Rectangle()
-                .foregroundStyle(gradient2)
-                .ignoresSafeArea()
-            
-            VStack {
+        NavigationStack {
+
+            ZStack{
                 
-                Spacer()
-                    .padding()
+                Rectangle()
+                    .foregroundStyle(gradient2)
+                    .ignoresSafeArea()
                 
-                TitleView()
-                
-                HStack {
-                    Image("longShirt")
-                        .resizable()
-                        .scaledToFit()
-                        .foregroundColor(.darkBlue)
-                        .frame(height: 150)
-                        .padding(.leading, 30)
+                VStack {
                     
                     Spacer()
-                
-                }
-                
-                HStack {
-                    Image("toothbrush")
-                        .resizable()
-                        .scaledToFit()
-                        .foregroundColor(.darkBlue)
-                        .frame(height: 120)
-                        .padding(.leading, 100)
-                }
-                
-                HStack {
-                    Image("pants")
-                        .resizable()
-                        .scaledToFit()
-                        .foregroundColor(.darkBlue)
-                        .frame(height: 150)
-                        .padding(.leading, 30)
-            
-               
+                        .padding()
                     
+                    TitleView()
+                    
+                    HStack {
+                        Image("longShirt")
+                            .resizable()
+                            .scaledToFit()
+                            .foregroundColor(.darkBlue)
+                            .frame(height: 150)
+                            .padding(.leading, 30)
+                        
+                        Spacer()
+                    
+                    }
+                    
+                    HStack {
+                        Image("toothbrush")
+                            .resizable()
+                            .scaledToFit()
+                            .foregroundColor(.darkBlue)
+                            .frame(height: 120)
+                            .padding(.leading, 100)
+                    }
+                    
+                    HStack {
+                        Image("pants")
+                            .resizable()
+                            .scaledToFit()
+                            .foregroundColor(.darkBlue)
+                            .frame(height: 150)
+                            .padding(.leading, 30)
+                
+                   
+                        
+                        Spacer()
+                        
+                           
+                    }
+                   
                     Spacer()
+                        .padding()
+                  
                     
-                       
+                    NavigationLink {
+                        FirstQuestionView()
+                    } label: {
+                        ZStack {
+                            Rectangle()
+                                .foregroundStyle(.darkBlue)
+                                .frame(width: 150)
+                                .cornerRadius(120)
+                            
+                            
+                            Text("Get Started")
+                                .foregroundColor(.white)
+                            
+                        }
+                        
+                    }
+
+                    
+
+                    Spacer()
+                        .padding()
+                    
                 }
-               
-                Spacer()
-                    .padding()
-              
+                    
                 
-                ZStack {
-                    Rectangle()
-                        .foregroundStyle(.darkBlue)
-                        .frame(width: 150)
-                        .cornerRadius(120)
-                    
-                    Text("Get Started")
-                        .foregroundColor(.white)
-                    
+                
                 }
-                
-                Spacer()
-                    .padding()
-                
             }
-                
-            
-            
-            }
+
         }
        
     
